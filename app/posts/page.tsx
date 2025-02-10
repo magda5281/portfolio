0f78b4dc8d1ec39a08c895e1eb5baf4f87910080
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-10 21:30:57 +0000

    Add PostsWithSearch

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,4 +1,4 @@
-import Posts from '@/components/posts'
+import PostsWithSearch from '@/components/postsWithSearch'
 import { getPosts } from '@/lib/posts'
 import React from 'react'
 
@@ -8,7 +8,7 @@ export default async function PostsPage() {
     <section className='pb-24 pt-40'>
       <div className='container max-w-3xl'>
         <h1 className='title mb-12'>Posts</h1>
-        <Posts posts={posts} />
+        <PostsWithSearch posts={posts} />
       </div>
     </section>
   )
