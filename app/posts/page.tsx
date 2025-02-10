60f42eaa9650cf150507dcd0f7658bf3ea8845da
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-10 20:55:46 +0000

    Add PostsPage content
    Created PostsPage
    Created Posts components
    Created getPosts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,15 @@
+import Posts from '@/components/posts'
+import { getPosts } from '@/lib/posts'
 import React from 'react'
 
-export default function PostsPage() {
-  return <div>All Posts Page</div>
+export default async function PostsPage() {
+  const posts = await getPosts()
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Posts</h1>
+        <Posts posts={posts} />
+      </div>
+    </section>
+  )
 }
---
+++ components/posts.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { PostMetadata } from '@/lib/posts'
+import Link from 'next/link'
+import { formatDate } from '@/lib/utils'
+
+export default function Posts({ posts }: { posts: PostMetadata[] }) {
+  return (
+    <ul className='flex flex-col gap-8'>
+      {posts.map(post => (
+        <li key={post.slug}>
+          <Link
+            href={`/posts/${post.slug}.mdx`}
+            className='gap-x4 flex justify-between gap-y-1'
+          >
+            <div className='max-w-lg'>
+              <p className='semibold text-lg'>{post.title}</p>
+              <p className='mt-1 line-clamp-2 text-sm font-light text-muted-foreground'>
+                {post.summary}
+              </p>
+            </div>
+            {post.publishedAt && (
+              <p className='mt-1 text-sm font-light'>
+                {formatDate(post.publishedAt)}
+              </p>
+            )}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
