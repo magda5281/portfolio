e802b4f4a2dff13d2419c136d5b7677a827d8557
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-11 15:17:22 +0000

    Add recent project to homepage

---
+++ components/recent-projects.tsx
@@ -0,0 +1,19 @@
+import { getProjects } from '@/lib/projects'
+import React from 'react'
+import Link from 'next/link'
+import Projects from '@/components/projects'
+
+export default async function RecentProjects() {
+  const projects = await getProjects(4)
+  return (
+    <section className='pb-24'>
+      <div>
+        <h2 className='title mb-12'>Recent projects</h2>
+        <Projects projects={projects} />
+        <Link href='/posts' className='mt-8 inline-flex items-center'>
+          <span>All projects</span>
+        </Link>
+      </div>
+    </section>
+  )
+}
