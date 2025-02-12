b0956a74d2c4931c3a5050ea40ca9d2f3ba36d02
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-02-12 13:31:48 +0000

    Add contact form
    Added form schema
    Added zod schema validation

---
+++ components/contact-form.tsx
@@ -0,0 +1,135 @@
+'use client'
+import React from 'react'
+import { z } from 'zod'
+import Link from 'next/link'
+import { toast } from 'sonner'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ContactFormSchema } from '@/lib/schemas'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+
+type Inputs = z.infer<typeof ContactFormSchema>
+export default function ContactForm() {
+  console.log('useForm:', useForm)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting }
+  } = useForm<Inputs>({
+    resolver: zodResolver(ContactFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      message: ''
+    }
+  })
+
+  const processForm: SubmitHandler<Inputs> = async data => {
+    toast.success('Message sent successfully!')
+  }
+
+  return (
+    <section className='relative isolate'>
+      {/* Background pattern from tailwind ui components??*/}
+      <svg
+        className='absolute inset-0 -z-10 h-full w-full stroke-zinc-200 opacity-20'
+        aria-hidden='true'
+      >
+        <defs>
+          <pattern
+            id='83fd4e5a-9d52-42fc-97b6-718e5d7ee527'
+            width={200}
+            height={200}
+            x='50%'
+            y={-64}
+            patternUnits='userSpaceOnUse'
+          >
+            <path d='M100 200V.5M.5 .5H200' fill='none' />
+          </pattern>
+        </defs>
+        <svg
+          x='50%'
+          y={-64}
+          className='overflow-visible fill-zinc-50 dark:fill-zinc-900/75'
+        ></svg>
+      </svg>
+
+      {/* Form */}
+      <div className='relative'>
+        <form
+          onSubmit={handleSubmit(processForm)}
+          className='mt-16 lg:flex-auto'
+          noValidate
+        >
+          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2'>
+            {/* Name */}
+            <div>
+              <Input
+                id='name'
+                type='text'
+                placeholder='Name'
+                autoComplete='given-name'
+                {...register('name')}
+              />
+              {errors.name?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+            {/* email */}
+            <div>
+              <Input
+                id='email'
+                type='text'
+                placeholder='email'
+                autoComplete='email'
+                {...register('email')}
+              />
+              {errors.email?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            {/* message */}
+
+            <div className='sm:col-span-2'>
+              <Textarea
+                rows={4}
+                placeholder='Message'
+                {...register('message')}
+              />
+
+              {errors.message?.message && (
+                <p className='ml-1 mt-2 text-sm text-rose-400'>
+                  {errors.message.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className='mt-6'>
+            <Button
+              type='submit'
+              disabled={isSubmitting}
+              className='w-full disabled:opacity-50'
+            >
+              {isSubmitting ? 'Submitting...' : 'Contact Us'}
+            </Button>
+          </div>
+
+          <p className='mt-4 text-xs text-muted-foreground'>
+            By submitting this form, I agree to the{' '}
+            <Link href='/privacy' className='font-bold'>
+              privacy&nbsp;policy.
+            </Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  )
+}
